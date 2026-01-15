112f1574312728b6d0f5a3e2b02164fe33474acd
sb13512 <sb13512@student.samk.fi>
2026-01-15 13:00:34 +0200

    Fixed the error related to login and then signup with urls and templates in base.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,12 @@
 from django.urls import path
 from .views import SignUpView
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('select-plan/<str:plan_type>/', views.update_plan, name='select_plan'),
 ]
